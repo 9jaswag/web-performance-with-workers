978d7a737a17322178b9dbe56e132e8bbfd6d993
Chuks Opia <chuks24ng@yahoo.co.uk>
2019-04-26 13:54:26 +0100

    chore: move animation logic to setInterval callback

--- index.js
+++ index.js
@@ -14,19 +14,15 @@ const move = () => {
   for (let i = 0; i < 3; i++) {
     const shuttleID = `shuttle${i + 1}`;
     let shuttle = document.getElementById(shuttleID);
-    animate(shuttle);
+
+    let position = 0;
+    setInterval(() => {
+      if (position > window.innerWidth / 1.2) {
+        position = 0;
+      } else {
+        position++;
+        shuttle.style.left = position + "px";
+      }
+    }, 5);
   }
 };
-
-const animate = (shuttle) => {
-  let position = 0;
-
-  setInterval(() => {
-    if (position > window.innerWidth / 1.2) {
-      position = 0;
-    } else {
-      position++;
-      shuttle.style.left = position + "px";
-    }
-  }, 5);
-}
